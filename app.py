9ccefaf34e7584bf3ce1f67ed5c08035135296fe
Shruthaja <mail.shruthaja@gmail.com>
2023-06-29 17:21:21 -0500

    fifty fifty

--- app.py
+++ app.py
@@ -22,11 +22,23 @@ def user():
     result = ""
     if request.method == "POST":
         pas=request.form['userpass']
-        regex = ('^(?=.*[A-Z]{2,})(?=.*\\d)(?=.*[#@+%-]).+$')
-        # Compile the ReGex
-        p = re.compile(regex)
-        print(p.match(pas))
-
+        con=open("conditions.txt",'r').read()
+        con=con.split()
+        result=""
+        forbid=['!','@','$','*']
+        for i in range(2,len(con)):
+            forbid.append(con[i])
+        if(len(pas)>=int(con[0]) and len(pas)<=int(con[1])):
+            regex = ('(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[@#$%^&+=])(?=\S+$).{8,}$')
+            p = re.compile(regex)
+            result=p.match(pas)
+            for i in forbid:
+                if i in pas:
+                    result=""
+            if(result==""):
+                result="does not meet pattern requirement"
+        else:
+                result="does not meet length"
     return render_template('index.html',result1=result)
 
 if __name__ == '__main__':
