732e483a160ec01b0b43b61114d1773b65c1705c
Shruthaja <mail.shruthaja@gmail.com>
2023-06-29 17:00:13 -0500

    fifty fifty

--- app.py
+++ app.py
@@ -22,26 +22,11 @@ def user():
     result = ""
     if request.method == "POST":
         pas=request.form['userpass']
-        print(pas)
-        con=str(open("conditions.txt", "r").read())
-        con=con.split(" ")
-        print(con)
-        l1=int(con[0])
-        l2=int(con[1])
-        if(len(pas)<l1 or len(pas)>l2):
-            result="Length does not meet requirements"
-        pat=['#','@','+','-','%']
-        for i in pat:
-            if i in pas:
-                result=result+"password ok"
-        if(result==""):
-            result=result+" pattern not matching"
-        notallowed=['!','$','*']
-        for i in range(2,len(con)):
-            notallowed.append(con[i])
-        for i in notallowed:
-            if i in pas:
-                result="contains not allowed patterns"
+        regex = ('^(?=.*[A-Z]{2,})(?=.*\\d)(?=.*[#@+%-]).+$')
+        # Compile the ReGex
+        p = re.compile(regex)
+        print(p.match(pas))
+
     return render_template('index.html',result1=result)
 
 if __name__ == '__main__':
